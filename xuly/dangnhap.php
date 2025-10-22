a9e1edd830fa1c078a87776f879d7893ab7847df
ngoquang1508 <ngovietquangpc@gmail.com>
2025-10-22 21:17:21 +0700

    sửa  lỗi đăng nhập

--- xuly/dangnhap.php
+++ xuly/dangnhap.php
@@ -47,5 +47,5 @@ $_SESSION['user'] = [
 
 // Chuyển trang theo role
 $redirect = ($user['role'] === 'admin') ? "../admin/" : "../?page=trangchu";
-header("Location: .. $redirect");
+header("Location: $redirect");
 exit;
