a0e67d527ede60af42841ea8c7faaa648c26e5b1
ngoquang1508 <ngovietquangpc@gmail.com>
2025-10-22 19:43:20 +0700

    push config db cho Tuấn=))

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+$env = parse_ini_file(__DIR__ . '/.env');
+
+$conn = new mysqli($env['DB_HOST'], $env['DB_USER'], $env['DB_PASS'], $env['DB_NAME']);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
