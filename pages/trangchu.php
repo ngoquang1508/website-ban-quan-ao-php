cfa8ddfb0e05bc0ea26af5ab61f47e1a01ebaa8c
TuanAnh86-dev <vuongcongtuan2005@gmail.com>
2025-11-12 20:22:38 +0700

    catalog

--- pages/trangchu.php
+++ pages/trangchu.php
@@ -1,5 +1,66 @@
-<div>
-    
-    <h1>trang chủ</h1>
-    
-</div>
+<!-- CSS -->
+<link rel="stylesheet" href="/assets/css/trangchu.css">
+
+<!-- BLOCK -->
+<section class="category-round">
+    <!-- ELEMENT -->
+    <ul class="category-round__list">
+
+        <li class="category-round__item">
+            <div class="category-round__thumb">
+                <img src="https://i.imgur.com/9jX5gZk.png" alt="Áo nữ">
+            </div>
+            <span class="category-round__label">Áo nữ</span>
+        </li>
+
+        <li class="category-round__item">
+            <div class="category-round__thumb">
+                <img src="https://i.imgur.com/2kR5iYp.png" alt="Váy">
+            </div>
+            <span class="category-round__label">Váy</span>
+        </li>
+
+        <li class="category-round__item">
+            <div class="category-round__thumb">
+                <img src="https://i.imgur.com/2qD5vJ8.png" alt="Áo nam">
+            </div>
+            <span class="category-round__label">Áo nam</span>
+        </li>
+
+        <li class="category-round__item">
+            <div class="category-round__thumb">
+                <img src="https://i.imgur.com/8nL3pZm.png" alt="Sơ mi">
+            </div>
+            <span class="category-round__label">Sơ mi</span>
+        </li>
+
+        <li class="category-round__item">
+            <div class="category-round__thumb">
+                <img src="https://i.imgur.com/FkR9pLm.png" alt="Quần">
+            </div>
+            <span class="category-round__label">Quần</span>
+        </li>
+
+        <li class="category-round__item">
+            <div class="category-round__thumb">
+                <img src="https://i.imgur.com/JpL5vXy.png" alt="Áo khoác">
+            </div>
+            <span class="category-round__label">Áo khoác</span>
+        </li>
+
+        <li class="category-round__item">
+            <div class="category-round__thumb">
+                <img src="https://i.imgur.com/5mK8vRt.png" alt="Giày dép">
+            </div>
+            <span class="category-round__label">Giày dép</span>
+        </li>
+
+        <li class="category-round__item">
+            <div class="category-round__thumb">
+                <img src="https://i.imgur.com/7kP9qWs.png" alt="Phụ kiện">
+            </div>
+            <span class="category-round__label">Phụ kiện</span>
+        </li>
+
+    </ul>
+</section>
